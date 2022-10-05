e7cf582361e8a602b35feb5b73fd1c98007b3da3
NorSP <norbissilvera@gmail.com>
2022-10-05 19:04:33 +0000

    comentario

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,74 @@
-import React from "react";
-
+import React,{useState} from "react";
+import '../../styles/index.css';
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+
+
+
 
 //create your first component
 const Home = () => {
+	
+	const [ colorR, setColorR] = useState("no-luz");
+	const [ colorY, setColorY] = useState("no-luz");
+	const [ colorG, setColorG] = useState("no-luz");
+
+	function TrafficLight1 (){
+		
+		
+	    if (colorR === "no-luz") {
+				setColorR("luz")
+				setColorY("no-luz")
+				setColorG("no-luz")
+			
+		};
+				
+	
+	};
+
+	function TrafficLight2 (){
+		
+		
+	    if (colorY === "no-luz") {
+				setColorY("luz")
+				setColorR("no-luz")
+				setColorG("no-luz")
+			
+		};
+				
+	
+	};
+
+	function TrafficLight3 (){
+		
+		
+	    if (colorG === "no-luz") {
+				setColorG("luz")
+				setColorY("no-luz")
+				setColorR("no-luz")
+			
+		};
+				
+	
+	};
+	
+
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		
+		<div className="container w-25 bg-dark d-flex justify-content-center">
+		<div>
+			<div className={"m-5 caja rounded-circle bg-danger "+colorR} onClick={TrafficLight1}></div>
+			<div className={"m-5 caja rounded-circle bg-warning "+colorY} onClick={TrafficLight2}></div>
+			<div className={"m-5 caja rounded-circle bg-success "+colorG} onClick={TrafficLight3}></div>
 		</div>
+	</div>
+	
+		
+		
+	
 	);
 };
 
